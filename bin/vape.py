a04427ef068cb0fa8328231d8410d0da2a4e30e9
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-05-05 14:29:40 +0100

    make skeleton project structure

---
+++ bin/vape.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from vape import vape_runner
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Filter, annotate and prioritize variants.',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser._action_groups.pop()
+    required_args = parser.add_argument_group('Required Arguments')
+    optional_args = parser.add_argument_group('Optional Arguments')
+    file_args = parser.add_argument_group('Annotation File Arguments')
+    filter_args = parser.add_argument_group('Variant Filtering  Arguments')
+
+    #required arguments
+    required_args.add_argument('-i', '--input', required=True, metavar='VCF',
+                               help='''Input VCF filename
+
+''')
+    #misc optional arguments
+    optional_args.add_argument('-o', '--output', type=argparse.FileType('w'), 
+                               default=sys.stdout, help=
+'''Output filename "Default = STDOUT
+
+''')
+    #args for filtering/retaining variants based on features
+    filter_args.add_argument('-f', '--freq', type=float, 
+                             help=
+'''Allele frequency cutoff (between 0 and 1). Used 
+for extenal allele frequency sources such as 
+--dbSNP or --gnomAD files.
+
+''')
+    filter_args.add_argument('--gq', type=int, 
+                             help=
+'''Minimum genotype quality score threshold. Genotype 
+calls with a score lower than this threshold will 
+be treated as no-calls
+
+''')
+
+    filter_args.add_argument('-v', '--variant_quality', type=float, 
+                             metavar='QUAL',
+                             help=
+'''Minimum variant quality score (QUAL 'field). 
+Variants with a QUAL score below this value will be 
+filtered/ignored.
+
+''') 
+    filter_args.add_argument('-p', '--pass_filters', action='store_true', 
+                             help=
+'''Only keep variants that have passed passed filters 
+(i.e. FILTER field must be "PASS")
+
+''' )
+    filter_args.add_argument('-c', '--csq', nargs='*', 
+                             help=
+'''One or more VEP consequence classes to  retain. 
+Variants which do not result in  one of these VEP 
+consequence classes will be filtered.  You may pass
+the value  "default" in order to include the  
+following classes: 
+                  TFBS_ablation
+                  TFBS_amplification
+                  inframe_deletion
+                  inframe_insertion
+                  frameshift_variant
+                  initiator_codon_variant
+                  missense_variant
+                  protein_altering_variant
+                  regulatory_region_ablation
+                  regulatory_region_amplification
+                  splice_acceptor_variant
+                  splice_donor_variant
+                  stop_gained
+                  stop_lost
+                  transcript_ablation
+                  transcript_amplification
+
+''' ) 
+    filter_args.add_argument('--canonical', action='store_true', 
+                             help=
+'''When used in conjunction with --csq  argument, 
+only consider functional  consequences of canonical 
+transcripts.
+
+''')
+
+
+    #args for specifying files for annotations/filtering
+    file_args.add_argument('-g', '--gnomAD', metavar='VCF', help=
+'''gnomAD/ExAC file for variant annotating/filtering
+using population allele frequencies
+
+''')
+    file_args.add_argument('-d', '--dbSNP', metavar='VCF', help=
+'''dbSNP file for variant annotating/filteirng
+
+''')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    vape_runner.run(args)
---
+++ vape/vape_runner.py
@@ -0,0 +1,8 @@
+from vape.parse_vcf import * 
+from Bio import bgzf
+
+def run(args):
+    ''' Run VCF filtering/annotation using args from bin/vape.py '''
+
+    print("This is a placeholder function")
+    pass
