9746028bba541960c17914346eaab49051d13af3
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-05-08 13:59:31 +0100

    add skeleton workflow

--- vape/vape_runner.py
+++ vape/vape_runner.py
@@ -1,8 +1,44 @@
-from vape.parse_vcf import * 
+import sys
+sys.path.insert(0, '')
+from vape.parse_vcf.parse_vcf import * 
 from Bio import bgzf
 
-def run(args):
+def run(vape_args):
     ''' Run VCF filtering/annotation using args from bin/vape.py '''
 
-    print("This is a placeholder function")
+    vcf_input = VcfReader(vape_args.input)
+    output = get_output(vape_args)
+    filters = get_filter_methods(vape_args)
+    print_header(vcf_input, output, vape_args)
+    for record in vcf_input.parser:
+        process_record(record)
+    finish_up()
+    output.close()
+
+def process_record(record):
     pass
+
+def finish_up():
+    pass
+
+def get_filter_methods(vape_args):
+    pass
+
+def print_header(vcf, out, vape_args):
+    vape_opts = []
+    for k,v in vars(vape_args).items():
+        vape_opts.append('--{} {}'.format(k, v))
+    prog_header = '##vape.py="' + str.join(" ", vape_opts) + '"'
+    out.write(str.join("\n", vcf.header.meta_header + [prog_header]) + "\n")
+    out.write(str.join("\t", vcf.col_header) + "\n")
+
+def get_output(vape_args):
+    fh = None
+    if isinstance(vape_args.output, str):
+        if vape_args.output.endswith(('.gz', '.bgz')):
+            fh = bgzf.BgzfWriter(vape_args.output)
+        else:
+            fh = open(vape_args.output, 'w')
+    else:
+        fh = sys.stdout
+    return fh
