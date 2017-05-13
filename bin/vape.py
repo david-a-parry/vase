5dddaaf85c13740abe3a21d1eb3d85a18a78b7f1
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-05-13 17:38:39 +0100

    add in silico options

--- bin/vape.py
+++ bin/vape.py
@@ -131,12 +131,72 @@ Use this argument to specify one or more biotypes
 to consider instead of those listed above. You may
 also include the value 'default' in your list to
 include the default values listed above in
-addition to those provided to this argument.
+addition to others provided to this argument.
 Alternatively you may use the value 'all' to
 disable filtering on biotypes.
 
 ''')
+    filter_args.add_argument( 
+'-m', '--missense_filters', default=[], nargs='+', help=
+'''A list of in silico prediction programs to use
+use for filtering missense variants (must be used
+in conjunction with --csq argument). The programs 
+provided her must have been annotated on the 
+input VCF file either directly by VEP or via the 
+dbNSFP VEP plugin. Recognised program names and 
+default 'damaging' values are provided in the
+"data/vep_insilico_pred.tsv" file. 
 
+You may optionally specify score criteria for 
+filtering as in the the following examples:
+                    
+    FATHMM_pred=D
+    MutationTaster_pred=A
+    MetaSVM_rankscore=0.8
+
+Or you may just provide the program names 
+and the default 'damaging' prediction values 
+will be used, as listed in the file
+"../data/vep_insilico_pred.tsv".
+
+By default, a missense consequence is filtered 
+unless each of the programs listed here have an 
+appropriate or missing prediction/score. This 
+behaviour can be changed using the 
+--filter_unpredicted or --keep_if_any_damaging 
+flags.
+
+''')
+    filter_args.add_argument(
+'--filter_unpredicted', action='store_true', default=False, help=
+'''For use in conjunction with --missense_filters. 
+The default behaviour when using 
+--missense_filters is to ignore a program if 
+there is no prediction given (i.e. the score/pred 
+is empty). That is, if there are no predictions 
+for any of the programs annotating a missense 
+consequence, it will not be filtered, while if 
+predictions are missing for only some, filtering 
+will proceed as normal with the other programs. If
+this option is given, missense variants will be 
+filtered if any program does not have a
+prediction/score.
+
+''')
+    filter_args.add_argument(
+'--keep_if_any_damaging', action='store_true', default=False, help=
+'''For use in conjunction with --missense_filters. 
+If this option is provided, a missense consequence
+is only filtered if ALL of the programs provided 
+to --missense_filters do not have an appropriate 
+prediction/score - that is, the missense 
+consequence will be retained if ANY of the given 
+programs has an appropriate value for the 
+prediction/score. This behaviour is overridden by
+'--filter_unpredicted' when a prediction/score is 
+missing for any program.
+
+''')
 
     #args for specifying files for annotations/filtering
     file_args.add_argument(
