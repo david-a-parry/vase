bef86716d609c9ba9d048dd506fa7a2b05fde6e2
David A. Parry <david.parry@ed.ac.uk>
2018-03-09 17:30:23 +0000

    count alleles from genotypes

--- vase/burden_counter.py
+++ vase/burden_counter.py
@@ -7,7 +7,7 @@ class BurdenCounter(object):
     '''
     
     def __init__(self, vcf, output, gq=0, dp=0, het_ab=0., hom_ab=0., 
-                use_ac=False, gnomad_pop=None, cases=[], controls=[]):
+                 is_gnomad=False, cases=[], controls=[]):
         self.vcf = vcf
         if 'SYMBOL' in vcf.header.csq_fields:
             self.gene_field = 'SYMBOL'
@@ -19,80 +19,101 @@ class BurdenCounter(object):
         self.min_dp = dp
         self.use_ac = use_ac
         self.samples = None
-        self.total_case_alleles = 0
-        self.total_control_alleles = 0
+        self.use_ac = False
+        self.total_alleles = {'Cases': 0, 'Controls': 0}
         if cases or controls:
             if not vcf.header.samples:
                 raise RuntimeError("No samples defined in VCF header - " +
                                    "cannot use --cases or --controls " + 
                                     "arguments.")
-            #warn that use_ac is disabled?
-            self.use_ac = False
             self.samples = cases + controls
-            self.total_case_alleles = len(cases) * 2
-            self.total_control_alleles = len(controls) * 2
+            self.total_alleles['Cases'] = len(cases) * 2
+            self.total_alleles['Controls'] = len(controls) * 2
+        elif not vcf.header.samples and not is_gnomad:
+            self.use_ac = True
         self.controls = controls
         self.cases = cases
-        self.gt_filter = GtFilter(vcf, gq=gq, dp=dp, het_ab=het_ab, 
-                                  hom_ab=hom_ab)
         self.gt_fields = self.gt_filter.fields
-        .or x in cases + controls:
+        for x in cases + controls:
             if x not in vcf.header.samples:
                raise RuntimeError("Burden counter sample '{}' not found in "
                                   .format(x) + "VCF input.")
-        self.gnomad_pop = None
-        if gnomad_pop:
-            self._check_gnomad_pop(vcf, gnomad_pop)
-            self.gnomad_pop = gnomad_pop
-        self.feat_to_cases = defaultdict(set)
-        self.feat_to_controls = defaultdict(set)
+        self.gnomad_pops = []
+        if is_gnomad:
+            self.gnomad_pops = self._check_gnomad_pops(vcf)
+            for pop in self.gnomad_pops:
+                self.total_alleles.clear()
+                self.total_alleles[pop] = 0
+        elif not self.use_ac:
+            self.gt_filter = GtFilter(vcf, gq=gq, dp=dp, het_ab=het_ab, 
+                                      hom_ab=hom_ab)
+        self.feat_to_cases = defaultdict(dict)
+        self.feat_to_controls = defaultdict(dict)
         self.transcript_to_gene = dict()
-        self.case_counts = defaultdict(int)
-        self.control_counts = defaultdict(int)
+        self.counts = defaultdict(dict)
         self.current_features = set()
         self.out_fh = open(output, 'wt')
         self.write_header()
 
     def write_header(self):
         cols = ["Feature", "Gene", ]
-        if self.controls:
-            cols.extend(["Controls", "N_Controls"])
-            if self.cases
+        if self.gnomad_pops:
+            for pop in self.gnomad_pops:
+                cols.extend([pop, "N_" + pop])
+        if self.cases or self.controls:
+            if self.cases:
                 cols.extend(["Cases", "N_Cases"])
-        elif self.gnomad_pop:
-            cols.extend([self.gnomad_pop, "N_" + self.gnomad_pop])
-        else:
+            if self.controls:
+                cols.extend(["Controls", "N_Controls"])
+        else: 
+            #all samples labelled as 'Cases' if neither cases or controls given 
             cols.extend(["Cases", "N_Cases"])
         self.out_fh.write(str.join("\t", cols) + "\n")
 
     def _check_gnomad_pop(vcf, pop):
         ac = 'AC_' + pop
         an = 'AN_' + pop
+        pop_ac_re = re.compile(r'''^AC_([A-Z]+)$''')
         expected = {ac: 'A', an: 1}
-        for f in [ac, an]:
-            if f in self.vcf.metadata['INFO']:
-                num = self.vcf.metadata['INFO'][f][-1]['Number']
-                typ = self.vcf.metadata['INFO'][f][-1]['Type']
-                if typ != 'Integer':
-                    raise RuntimeError("Unexpected 'Type' identifier '{}' "
-                                       .format(typ) + "for INFO field {}"
-                                       .format(f) + "in input VCF header.\n")
-                if num != expected[f]:
-                    raise RuntimeError("Unexpected 'Number' identifier '{}' "
-                                       .format(num) + "for INFO field {}"
-                                       .format(f) + "in input VCF header.\n")
+        pops = []
+        for f in self.vcf.metadata['INFO']:
+            match = pop_ac_re.match(f)
+            if match:
+                p = match.groups(1)
+                an = 'AN_' + p
+                ac_num = self.vcf.metadata['INFO'][f][-1]['Number']
+                ac_typ = self.vcf.metadata['INFO'][f][-1]['Type']
+                an_num = self.vcf.metadata['INFO'][an][-1]['Number']
+                an_typ = self.vcf.metadata['INFO'][an][-1]['Type']
+                if (ac_num == 'A' and ac_type == 'Integer' and 
+                    an_num == 1 and ac_type == 'Integer'):
+                    pops.append(p)
+        if not pops:
+            raise RuntimeError("No gnomAD populations found for VCF input!")
+        pops.sort()
+        return pops
     
     def count(record, ignore_alleles=[], ignore_csq=[]):
-        if not self.use_ac and not self.gnomad_pop:
+        if not self.use_ac and not self.gnomad_pops:
             these_feats = set([x['Feature'] for x in record.CSQ])
             if (self.self.current_features and these_feats.isdisjoint(
                 self.current_features)):
                 # if we've moved on to next set of features clear feat_to_cases 
                 # etc. and add sample counts 
                 for feat in self.feat_to_cases:
-                    self.case_counts[feat] += len(self.feat_to_cases[feat])
+                    self.counts[feat]['Cases'] = sum(
+                        self.feat_to_cases[feat][x] for x in 
+                        self.feat_to_cases[feat])
                     del self.feat_to_cases[feat]
-                    self.control_counts[feat] += len(self.feat_to_controls[feat])
+                    self.counts[feat]['Controls'] = sum(
+                        self.feat_to_controls[feat][x] for x in 
+                        self.feat_to_controls[feat])
+                    del self.feat_to_controls[feat]
+                for feat in (x for x in self.feat_to_controls if x not in
+                             self.feat_to_cases):
+                    self.counts[feat]['Controls'] = sum(
+                        self.feat_to_controls[feat][x] for x in 
+                        self.feat_to_controls[feat])
                     del self.feat_to_controls[feat]
                 self.current_features.clear()
             self.current_features.update(these_feats)
@@ -119,61 +140,71 @@ class BurdenCounter(object):
             self.feat_to_cases/controls set, otherwise add number of 
             alleles.
         '''
-        if self.gnomad_pop:
-            g_ac = 'AC_' + self.gnomad_pop
-            g_an = 'AN_' + self.gnomad_pop
-            info = record.parsed_info_fields(fields=[g_ac, g_an])
-            if info[g_ac][allele] is not None:
-                self.case_counts[feat] += info[g_ac][allele]
-            if info[g_an][allele] is not None:
-                if info[g_an][allele] > self.total_case_alleles:
-                    self.total_case_alleles = info[g_an][allele]
+        if self.gnomad_pops:
+            for pop in self.gnomad_pops:
+                g_ac = 'AC_' + self.gnomad_pop
+                g_an = 'AN_' + self.gnomad_pop
+                info = record.parsed_info_fields(fields=[g_ac, g_an])
+                if info[g_ac][allele] is not None:
+                    self.counts[feat][pop] += info[g_ac][allele]
+                if info[g_an] is not None:
+                    if info[g_an] > self.total_alleles[pop]:
+                        self.total_alleles[pop] = info[g_an]
         elif self.use_ac:
             info = record.parsed_info_fields(fields=['AC', 'AN'])
-            self.case_counts[feat] += info['AC'][allele]
-            if info['AN'][allele] is not None:
-                if info['AN'][allele] > self.total_case_alleles:
-                    self.total_case_alleles = info['AN'][allele]
+            self.counts[feat]['Cases'] += info['AC'][allele]
+            if info['AN'] is not None:
+                if info['AN'] > self.total_alleles['Cases']:
+                    self.total_alleles['Cases'] = info['AN']
         else:
             gts = record.parsed_gts(fields=self.gt_fields, samples=self.samples)
-            # TODO!
-            # major limitation here is that we don't count how many alleles per
-            # sample - i.e. if homozygous. Maybe create a dict of samples to 
-            # allele counts?
             if cases or controls:
                 for s in cases:
                     if self.gt_filter.gt_is_ok(gts, s, allele):
-                        if allele in gts['GT'][s]:
-                            self.feat_to_cases[feat].add(s)
+                        ac = gts['GT'][s].count(allele)
+                        if s in self.feat_to_cases[feat]:
+                            self.feat_to_cases[feat][s] += ac
+                            #do not count more than 2 alleles per sample
+                            if self.feat_to_cases[feat][s] > 2:
+                                self.feat_to_cases[feat][s] = 2:
+                        else:
+                            self.feat_to_cases[feat][s] = ac
                 for s in controls:
                     if self.gt_filter.gt_is_ok(gts, s, allele):
-                        if allele in gts['GT'][s]:
-                            self.feat_to_controls[feat].add(s)
+                        ac = gts['GT'][s].count(allele)
+                        if s in self.feat_to_cases[feat]:
+                            self.feat_to_controls[feat][s] += ac
+                            if self.feat_to_controls[feat][s] > 2:
+                                #do not count more than 2 alleles per sample
+                                self.feat_to_controls[feat][s] = 2:
+                        else:
+                            self.feat_to_controls[feat][s] = ac
             else:
-                self.feat_to_cases.update(s for s in gts if 
-                  self.gt_filter.gt_is_ok(gts, s, allele) and allele in gts[s])
-    
+                self.feat_to_cases.update(gts['GT'][s].count(allele) for s in 
+                                          gts if self.gt_filter.gt_is_ok(
+                                          gts, s, allele) and allele in gts[s])
+
     def output_counts(self):
-        for feat in self.case_counts:
-            row = [feat, self.transcript_to_gene[feat]]
-            if self.controls:
-                if feat in self.control_counts:
-                    row.append(str(self.control_counts[feat]))
-                else:
-                    row.append("0")
-                row.append(self.total_control_alleles)
-            row.append(self.case_counts[feat])
-            row.append(self.total_case_alleles)
-            self.out_fh.write(str.join("\t", row) + "\n")
-        for feat in self.control_counts:
-            row.append(str(self.control_counts[feat]))
-            row.append(self.total_control_alleles)
+        groups = []
+        if self.gnomad_pops:
+            groups = self.gnomad_pops
+        elif self.use_ac:
+            groups = ['Cases']
+        elif self.cases or self.controls:
             if self.cases:
-                if feat in self.case_counts:
-                    row.append(self.case_counts[feat])
+                groups.append('Cases')
+            if self.controls:
+                groups.append('Controls')
+        else:
+            groups = ['Cases']
+        for feat in self.counts:
+            row = [feat, self.transcript_to_gene[feat]]
+            for g in groups:
+                if g in self.counts[feat]:
+                    row.append(str(self.counts[feat][g]))
                 else:
                     row.append("0")
-                row.append(self.total_case_alleles)
+                row.append(self.total_alleles[g])
             self.out_fh.write(str.join("\t", row) + "\n")
         self.out_fh.close()
         
