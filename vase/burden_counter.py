ddbcd193fd22bbe4c273b49e5949ff7d8156b536
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-03-12 17:09:16 +0000

    gnomad fixes

--- vase/burden_counter.py
+++ vase/burden_counter.py
@@ -1,5 +1,7 @@
+import re
 from .sample_filter import GtFilter
 from collections import defaultdict
+
 
 class BurdenCounter(object):
     ''' For a set of variants count the number of qualifying alleles
@@ -42,8 +44,8 @@ class BurdenCounter(object):
         self.gnomad_pops = []
         if is_gnomad:
             self.gnomad_pops = self._check_gnomad_pops(vcf)
+            self.total_alleles.clear()
             for pop in self.gnomad_pops:
-                self.total_alleles.clear()
                 self.total_alleles[pop] = 0
         elif not self.use_ac:
             self.gt_filter = GtFilter(vcf, gq=gq, dp=dp, het_ab=het_ab, 
@@ -62,7 +64,7 @@ class BurdenCounter(object):
         if self.gnomad_pops:
             for pop in self.gnomad_pops:
                 cols.extend([pop, "N_" + pop])
-        if self.cases or self.controls:
+        elif self.cases or self.controls:
             if self.cases:
                 cols.extend(["Cases", "N_Cases"])
             if self.controls:
@@ -72,23 +74,20 @@ class BurdenCounter(object):
             cols.extend(["Cases", "N_Cases"])
         self.out_fh.write(str.join("\t", cols) + "\n")
 
-    def _check_gnomad_pop(vcf, pop):
-        ac = 'AC_' + pop
-        an = 'AN_' + pop
+    def _check_gnomad_pops(self, vcf):
         pop_ac_re = re.compile(r'''^AC_([A-Z]+)$''')
-        expected = {ac: 'A', an: 1}
         pops = []
         for f in self.vcf.metadata['INFO']:
             match = pop_ac_re.match(f)
             if match:
-                p = match.groups(1)
+                p = match.group(1)
                 an = 'AN_' + p
                 ac_num = self.vcf.metadata['INFO'][f][-1]['Number']
                 ac_typ = self.vcf.metadata['INFO'][f][-1]['Type']
                 an_num = self.vcf.metadata['INFO'][an][-1]['Number']
                 an_typ = self.vcf.metadata['INFO'][an][-1]['Type']
-                if (ac_num == 'A' and ac_type == 'Integer' and 
-                    an_num == 1 and ac_type == 'Integer'):
+                if (ac_num == 'A' and ac_typ == 'Integer' and 
+                    an_num == '1' and an_typ == 'Integer'):
                     pops.append(p)
         if not pops:
             raise RuntimeError("No gnomAD populations found for VCF input!")
@@ -179,8 +178,8 @@ class BurdenCounter(object):
         a_counts = defaultdict(int)
         if self.gnomad_pops:
             for pop in self.gnomad_pops:
-                g_ac = 'AC_' + self.gnomad_pop
-                g_an = 'AN_' + self.gnomad_pop
+                g_ac = 'AC_' + pop 
+                g_an = 'AN_' + pop
                 info = record.parsed_info_fields(fields=[g_ac, g_an])
                 if info[g_ac][allele] is not None:
                     a_counts[pop] += info[g_ac][allele]
@@ -203,7 +202,10 @@ class BurdenCounter(object):
             self._check_gene_name(feat, record)
             if self.gnomad_pops or self.use_ac:
                 for group in a_counts:
-                    self.counts[feat][group] += a_counts[group]
+                    if group in self.counts[feat]:
+                        self.counts[feat][group] += a_counts[group]
+                    else:
+                        self.counts[feat][group] = a_counts[group]
             else:#if we have samples we can ensure we don't count twice
                 if self.cases or self.controls:
                     for s in self.cases:
