6f8c7c2b3f4549dbc7a8fa43b27420168e2885fd
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-04-04 11:38:59 +0100

    typo in script list

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
           'pysam',
           'parse_vcf>=0.2.1',
       ],
-    scripts = ["bin/vase", "bin/burden_test_vase," "bin/vase_reporter"],
+    scripts = ["bin/vase", "bin/burden_test_vase", "bin/vase_reporter"],
     classifiers = [
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
