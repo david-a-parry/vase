1c031de0ec37a85c007957a811ed1b3406a94c2a
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-04-25 22:38:24 +0100

    fix overlap checks and error message

--- vase/genomic_interval.py
+++ vase/genomic_interval.py
@@ -1,14 +1,14 @@
 
 
 class GenomicInterval(object):
-    ''' 
-        Simple class for representing a genomic interval, potentially 
+    '''
+        Simple class for representing a genomic interval, potentially
         merged from several overlapping BED regions. '''
 
     __slots__ = ['contig', 'start', 'end', 'regions']
 
     def __init__(self, interval):
-        ''' 
+        '''
             Args:
                 interval:   A list of columns from a BED file.
         '''
@@ -17,58 +17,58 @@ class GenomicInterval(object):
         self.end = int(interval[2])
         self.regions = [interval]
         if start >= end:
-            raise ValueError("BED interval can not be equal to or greater " + 
-                            "than end (for interval {}:{}-{})"
-                            .format(self.contig, self.start+1, self.end))    
+            raise ValueError("Start of BED interval can not be equal to or " +
+                             "greater than end (for interval {}:{}-{})"
+                             .format(self.contig, self.start+1, self.end))
 
     def __str__(self):
         return self.contig + ':' + str(self.start+1) + '-' + str(self.end)
 
     def __eq__(self, other):
-        return (self.contig == other.contig and self.start == other.start and 
-               self.end == other.end)
+        return (self.contig == other.contig and self.start == other.start and
+                self.end == other.end)
 
     def __ne__(self, other):
         return (self.contig != other.contig or self.start != other.start or
-               self.end != other.end)
+                self.end != other.end)
 
     def __lt__(self, other):
-        return (self.contig < other.contig or 
-               (self.contig == other.contig and 
-               (self.start < other.start or self.start == other.start and 
+        return (self.contig < other.contig or
+               (self.contig == other.contig and
+               (self.start < other.start or self.start == other.start and
                 self.end < other.end)))
 
     def __le__(self, other):
-        return (self.contig < other.contig or 
+        return (self.contig < other.contig or
                (self.contig == other.contig and self.end <= other.end))
 
     def __gt__(self, other):
-        return (self.contig > other.contig or 
-               (self.contig == other.contig and 
-               (self.start > other.start or self.start == other.start and 
+        return (self.contig > other.contig or
+               (self.contig == other.contig and
+               (self.start > other.start or self.start == other.start and
                 self.end > other.end)))
 
     def __ge__(self, other):
-        return (self.contig > other.contig or 
+        return (self.contig > other.contig or
                (self.contig == other.contig and self.start >= other.start))
 
     def overlaps(self, other):
         if self.contig != other.contig:
             return False
-        elif self.start < other.end and self.end > other.start: 
+        elif self.start < other.end and self.end > other.start:
             #start is 0-based so only overlaps if < end
             return True
         return False
 
     def merge_interval(self, other):
-        ''' 
+        '''
             Merge an overlapping interval.
 
             Args:
                 interval:   Another GenomicInterval object
 
         '''
-        if (other > self or other < self):
+        if not self.overlaps(other):
             raise NonOverlappingIntervalError("Can not merge non-overlapping" +
                     " intervals '{}' and '{}'".format(self, other))
         if other.start < self.start:
@@ -77,7 +77,7 @@ class GenomicInterval(object):
             self.end = other.end
         self.regions.extend(other.regions)
         self.regions.sort(key=operator.itemgetter(0, 1, 2, 3))
-        
+
 
 class NonOverlappingIntervalError(ValueError):
     pass
