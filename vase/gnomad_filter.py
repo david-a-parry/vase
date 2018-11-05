291eed102fce15bd42afb82d9f1915dced6135ff
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-11-05 13:09:34 +0000

    allow choice of gnomAD populations

--- vase/gnomad_filter.py
+++ vase/gnomad_filter.py
@@ -8,7 +8,7 @@ class GnomadFilter(VcfFilter):
         annotations in a gnomAD or ExAC VCF file.
     '''
 
-    def __init__(self, vcf, prefix, freq=None, min_freq=None):
+    def __init__(self, vcf, prefix, freq=None, min_freq=None, pops=None):
         '''
             Initialize object with a VCF file and optional filtering
             arguments.
@@ -26,14 +26,15 @@ class GnomadFilter(VcfFilter):
                 min_freq: Filter alleles if allele frequency is less
                           than this value. Optional.
 
-        '''
+                pops:     gnomAD population annotations to use. Default
+                          are AFR, AMR, EAS, FIN, NFE and SAS.
 
-        freq_info = ("AF_POPMAX", "AF_AFR", "AF_AMR", "AF_EAS", "AF_FIN",
-                       "AF_NFE", "AF_SAS")
-        ac_info = ("AC_POPMAX", "AC_AFR", "AC_AMR", "AC_EAS", "AC_FIN",
-                      "AC_NFE", "AC_SAS",)
-        an_info = ("AN_POPMAX", "AN_AFR", "AN_AMR", "AN_EAS", "AN_FIN",
-                    "AN_NFE", "AN_SAS",)
+        '''
+        if pops is None:
+            pops = ["AFR", "AMR", "EAS", "FIN", "NFE", "SAS"]
+        freq_info = ["AF_" + p for p in pops]
+        ac_info = ["AC_" + p for p in pops]
+        an_info = ["AN_" + p for p in pops]
         super().__init__(vcf, prefix, freq, min_freq, freq_info, ac_info,
                          an_info)
 
