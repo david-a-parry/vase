8a06ae3ceb40657b777ac18cb0596ad259ae3b4d
David A. Parry <david.parry@igmm.ed.ac.uk>
2019-03-07 14:42:45 +0000

    add --max_gnomad_homozygotes option; support v2.1 annotations

--- vase/gnomad_filter.py
+++ vase/gnomad_filter.py
@@ -8,7 +8,8 @@ class GnomadFilter(VcfFilter):
         annotations in a gnomAD or ExAC VCF file.
     '''
 
-    def __init__(self, vcf, prefix, freq=None, min_freq=None, pops=None):
+    def __init__(self, vcf, prefix, freq=None, min_freq=None, pops=None,
+                 max_homozygotes=None):
         '''
             Initialize object with a VCF file and optional filtering
             arguments.
@@ -29,13 +30,46 @@ class GnomadFilter(VcfFilter):
                 pops:     gnomAD population annotations to use. Default
                           are AFR, AMR, EAS, FIN, NFE and SAS.
 
+                max_homozygotes:
+                          Filter alleles if the total number of homozygotes
+                          or hemizygotes is equal to or greater than this
+                          value.
+
         '''
         if pops is None:
             pops = ["AFR", "AMR", "EAS", "FIN", "NFE", "SAS"]
-        freq_info = ["AF_" + p for p in pops]
-        ac_info = ["AC_" + p for p in pops]
-        an_info = ["AN_" + p for p in pops]
-        super().__init__(vcf, prefix, freq, min_freq, freq_info, ac_info,
-                         an_info)
+        #v2.1 put pops in lowercase, previously were in uppercase - allow both
+        g_pops = [x.lower() for x in pops] + [x.upper() for x in pops]
+        freq_info = ["AF_" + p for p in g_pops]
+        ac_info = ["AC_" + p for p in g_pops]
+        an_info = ["AN_" + p for p in g_pops]
+        hom_info = ["Hom_" + p for p in g_pops]
+        hemi_info = ["Hemi_" + p for p in g_pops]
+        super().__init__(vcf=vcf, prefix=prefix, freq=freq, min_freq=min_freq,
+                         freq_fields=freq_info, ac_fields=ac_info,
+                         an_fields=an_info, annotations=hom_info+hemi_info,
+                         allow_missing_annotations=True)
+        self.hom_annots = [self.prefix + "_" + f for f in self.extra if f in
+                           self.annot_fields]
+        self.max_homozygotes = max_homozygotes
+        if self.max_homozygotes is not None:
+            self.annotate_and_filter_record = self.filter_including_homozygotes
+        else:
+            self.annotate_and_filter_record = super().annotate_and_filter_record
 
 
+    def filter_including_homozygotes(self, record):
+        filter, keep, matched = super().annotate_and_filter_record(record)
+        if sum(filter) == len(filter):
+            #no need to check homozygotes - filtering anyway
+            return filter, keep, matched
+        hom_info = record.parsed_info_fields(self.hom_annots)
+        if hom_info:
+            for i in range(len(record.DECOMPOSED_ALLELES)):
+                if filter[i]:
+                    continue
+                hom_counts = sum(hom_info[x][i] for x in self.hom_annots if
+                                 hom_info[x][i] is not None)
+                if hom_counts > self.max_homozygotes:
+                    filter[i] = True
+        return filter, keep, matched
