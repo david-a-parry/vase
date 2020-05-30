ec3d25a6b701093807cc922546f20436c0771965
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-05-30 21:26:25 +0100

    Read from STDIN by default

--- test/utils/vcf2vars.py
+++ test/utils/vcf2vars.py
@@ -3,9 +3,12 @@ import pysam
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit("Usage: {} in.vcf".format(sys.argv[0]))
-    vcf = pysam.VariantFile(sys.argv[1])
+    if len(sys.argv) > 2:
+        sys.exit("Usage: {} [in.vcf]".format(sys.argv[0]))
+    if len(sys.argv) > 1:
+        vcf = pysam.VariantFile(sys.argv[1])
+    else:
+        vcf = pysam.VariantFile('-')
     pos_offset = 1000000
     for record in vcf:
         print("{}:{}-{}/{}".format(record.chrom,
