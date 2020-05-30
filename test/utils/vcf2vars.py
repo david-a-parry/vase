67a4e923046240b359f422c5507f409b8f5e2fbe
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-05-30 19:41:51 +0100

    Add variant summarizer test util script

---
+++ test/utils/vcf2vars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pysam
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit("Usage: {} in.vcf".format(sys.argv[0]))
+    vcf = pysam.VariantFile(sys.argv[1])
+    pos_offset = 1000000
+    for record in vcf:
+        print("{}:{}-{}/{}".format(record.chrom,
+                                   record.pos,
+                                   record.ref,
+                                   ','.join(record.alts)))
+    vcf.close()
