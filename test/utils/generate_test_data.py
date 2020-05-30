3c5630ecb199fda62a6c53c9872745828095fb82
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-05-30 12:35:12 +0100

    add test data and utils

---
+++ test/utils/generate_test_data.py
@@ -0,0 +1,27 @@
+import pysam
+import sys
+import random
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit("Usage: {} in.vcf sample1 [sample2...sampleN]".format(
+            sys.argv[0]))
+    vcf = pysam.VariantFile(sys.argv[1])
+    samples = sys.argv[2:]
+    vcf.subset_samples(samples)
+    out = pysam.VariantFile("-", mode='w', header=vcf.header)
+    pos_offset = 1000000
+    for record in vcf.fetch('16', 68514471, 72557468):
+        if any(y is not None and y > 0 for x in samples for y in
+               record.samples[x].allele_indices):
+            record.chrom = '1'
+            record.pos = pos_offset + random.randint(10, 1e3)
+            if random.randint(0, 10) % 2:
+                record.id = "snp{}".format(random.randint(1, 1e5))
+            else:
+                record.id = '.'
+            out.write(record)
+            pos_offset = record.pos
+    vcf.close()
+    out.close()
---
+++ test/utils/rewrite_csq.py
@@ -0,0 +1,57 @@
+import sys
+import re
+from parse_vcf import VcfReader
+
+csq_fields = ['Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene',
+              'Feature_type', 'Feature', 'BIOTYPE', 'EXON', 'INTRON'
+             ]
+csq_format_re = re.compile(r'''.*Format:\s*((\S+\|)*\S+)"''')
+
+transcripts = dict()
+id_conversions = {'SYMBOL': dict(),
+                  'Gene': dict(),
+                  'Feature': dict()}
+id_counts = {'SYMBOL': 0,
+             'Gene': 0,
+             'Feature': 0}
+
+def rewrite_csq(rec):
+    annots = []
+    for c in rec.CSQ:
+        for k, d in id_conversions.items():
+            if c[k] in d:
+                c[k] = d[c[k]]
+            elif c[k]:
+                id_counts[k] += 1
+                n = "{}_{}".format(k.upper(), id_counts[k])
+                id_conversions[k][c[k]] = n
+                c[k] = n
+        annots.append('|'.join(c[x] for x in c.keys() if x in csq_fields))
+    return ','.join(annots)
+
+
+if __name__ == '__main__':
+    vcf = VcfReader(sys.argv[1])
+    meta_data = []
+    for x in vcf.header.meta_header:
+        if x.startswith('##INFO=<ID=CSQ,'):
+            d = vcf.header.metadata['INFO']['CSQ'][0]
+            desc = d['Description']
+            match = csq_format_re.match(desc)
+            fields = match.group(1).split('|')
+            new_f = [x for x in fields if x in csq_fields]
+            new_desc = '"Consequence annotations from Ensembl VEP. Format: ' \
+                       + '|'.join(new_f)
+            s = str.join(',', ['##INFO' + "=<ID=" + 'CSQ'] +
+                         ["Number=" + d['Number'],
+                          "Type=" + d['Type'],
+                          "Description=" + new_desc]) + "\">"
+            meta_data.append(s)
+        else:
+            meta_data.append(x)
+    vcf.header.meta_header = meta_data
+    print(vcf.header, end='')
+    for rec in vcf:
+        csq = rewrite_csq(rec)
+        rec.add_info_fields(info={'CSQ': csq})
+        print(rec)
