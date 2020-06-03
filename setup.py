7a370e3ab4ef8bd6752aad2f068afa8bf37fd3da
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-06-03 11:44:03 +0100

    update setup script

--- setup.py
+++ setup.py
@@ -4,32 +4,35 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = "vase",
-    packages = ["vase"],
-    version = "0.2.5",
-    description = "Variant Annotation, Segregation and Exclusion",
-    author = "David A. Parry",
-    author_email = "david.parry@igmm.ed.ac.uk",
-    url = "https://github.com/david-a-parry/vase",
-    download_url = 'https://github.com/david-a-parry/vase/archive/0.2.5.tar.gz',
+    name="vase",
+    packages=["vase"],
+    version="0.3",
+    description="Variant Annotation, Segregation and Exclusion",
+    author="David A. Parry",
+    author_email="david.parry@igmm.ed.ac.uk",
+    url="https://github.com/david-a-parry/vase",
+    download_url='https://github.com/david-a-parry/vase/archive/0.3.tar.gz',
     license='MIT',
     install_requires=[
           'pysam',
-          'parse_vcf>=0.2.6',
           'natsort',
+    ],
+    test_requires=[
+        'nose',
+        'numpy'
     ],
     extras_require={
         'BGZIP': ['biopython'],
         'REPORTER': ['xlsxwriter', 'requests'],
         'MYGENEINFO': ['mygene'],
     },
-    scripts = ["bin/vase", "bin/burden_test_vase", "bin/vase_reporter",
-               "bin/coordinates_from_genes", "bin/filter_gts",
-               "bin/phase_by_transmission"],
+    scripts=["bin/vase", "bin/burden_test_vase", "bin/vase_reporter",
+             "bin/coordinates_from_genes", "bin/filter_gts",
+             "bin/phase_by_transmission"],
     include_package_data=True,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 3 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
