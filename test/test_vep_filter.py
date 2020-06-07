3e7e0c01765471f8e47e0247325ab06bb76649f2
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-06-07 13:36:54 +0100

    add additional CSQ tests

--- test/test_vep_filter.py
+++ test/test_vep_filter.py
@@ -1,4 +1,6 @@
 from .utils import *
+from vase.vep_filter import VepFilter
+from vase.vcf_reader import VcfReader
 
 is_input = os.path.join(dir_path, 'test_data', 'ex5.bcf')
 
@@ -130,6 +132,105 @@ def test_insilico_keep_any_damaging():
                                  sys._getframe().f_code.co_name)
     assert_equal(results, expected)
     os.remove(output)
+
+
+def test_canonical():
+    vcf = VcfReader(is_input)
+    csq_filter = VepFilter(
+        vcf=vcf,
+        csq=[],
+        canonical=True)
+    results_alts = []
+    results_csq = []
+    expected_alts = [[False]] * 7
+    expected_csq = [[False, True, True, True],
+                    [False, True, True, True],
+                    [False, True, True, True],
+                    [False, False, True, False],
+                    [False, True, True, True],
+                    [False, True, True, True],
+                    [False, True, True, True]]
+    for record in vcf:
+        r_alts, r_csq = csq_filter.filter(record)
+        results_csq.append(r_csq)
+        results_alts.append(r_alts)
+    assert_equal(expected_csq, results_csq)
+    assert_equal(expected_alts, results_alts)
+
+
+def test_flags():
+    vcf = VcfReader(is_input)
+    csq_filter = VepFilter(
+        vcf=vcf,
+        csq=['all'],
+        biotypes=['all'],
+        filter_flagged_features=True)
+    results_alts = []
+    results_csq = []
+    expected_alts = [[False]] * 7
+    expected_csq = [[False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, True, False, False],
+                    [False, True, False, False]]
+    for record in vcf:
+        r_alts, r_csq = csq_filter.filter(record)
+        results_csq.append(r_csq)
+        results_alts.append(r_alts)
+    assert_equal(expected_csq, results_csq)
+    assert_equal(expected_alts, results_alts)
+
+
+
+def test_canonical_stop_gained():
+    vcf = VcfReader(is_input)
+    csq_filter = VepFilter(
+        vcf=vcf,
+        csq=['stop_gained'],
+        canonical=True)
+    results_alts = []
+    results_csq = []
+    expected_alts = [[True]] * 5 + [[False]] * 2
+    expected_csq = [[True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [False, True, True, True],
+                    [False, True, True, True]]
+    for record in vcf:
+        r_alts, r_csq = csq_filter.filter(record)
+        results_csq.append(r_csq)
+        results_alts.append(r_alts)
+    assert_equal(expected_csq, results_csq)
+    assert_equal(expected_alts, results_alts)
+
+
+def test_canonical_lof():
+    vcf = VcfReader(is_input)
+    csq_filter = VepFilter(
+        vcf=vcf,
+        csq=['stop_gained'],
+        loftee=True,
+        canonical=True)
+    results_alts = []
+    results_csq = []
+    expected_alts = [[True]] * 5 + [[False]] + [[True]]
+    expected_csq = [[True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [True, True, True, True],
+                    [False, True, True, True],
+                    [True, True, True, True]]
+    for record in vcf:
+        r_alts, r_csq = csq_filter.filter(record)
+        results_csq.append(r_csq)
+        results_alts.append(r_alts)
+    assert_equal(expected_csq, results_csq)
+    assert_equal(expected_alts, results_alts)
 
 
 if __name__ == '__main__':
