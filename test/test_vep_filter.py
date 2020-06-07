4a952e9cb6f3a8281f0da4518d5f5f3977d19deb
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-06-07 12:35:49 +0100

    move in silico tests into vep_filter tests file

--- test/test_vep_filter.py
+++ test/test_vep_filter.py
@@ -1,5 +1,7 @@
 from .utils import *
 
+is_input = os.path.join(dir_path, 'test_data', 'ex5.bcf')
+
 
 def test_csq():
     output = get_tmp_out()
@@ -18,6 +20,111 @@ def test_impact():
     test_args = dict(
         impact=["HIGH"],
         output=output,
+    )
+    results, expected = run_args(test_args, output,
+                                 sys._getframe().f_code.co_name)
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_freq():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        freq=0.01,
+    )
+    results, expected = run_args(test_args, output,
+                                 sys._getframe().f_code.co_name)
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_pred():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        missense_filters=['sift=deleterious'],
+    )
+    results, expected = run_args(test_args, output,
+                                 'test_insilico_pred_sift')
+    assert_equal(results, expected)
+    os.remove(output)
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        missense_filters=['Polyphen'])
+    results, expected = run_args(test_args, output,
+                                 'test_insilico_pred_polyphen')
+    assert_equal(results, expected)
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        missense_filters=['Polyphen', 'sift=deleterious'],
+    )
+    results, expected = run_args(test_args, output,
+                                 'test_insilico_pred_both')
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_freq_pred():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        freq=0.01,
+        missense_filters=['sift=deleterious'],
+    )
+    results, expected = run_args(test_args, output,
+                                 sys._getframe().f_code.co_name)
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_score():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        missense_filters=['SIFT_score=0.1'])
+    results, expected = run_args(test_args, output,
+                                 'test_insilico_score')
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_unpredicted():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        filter_unpredicted=True,
+        missense_filters=['SIFT_score=0.1'])
+    results, expected = run_args(test_args, output,
+                                 'test_insilico_unpredicted')
+    assert_equal(results, expected)
+    os.remove(output)
+
+
+def test_insilico_keep_any_damaging():
+    output = get_tmp_out()
+    test_args = dict(
+        input=is_input,
+        output=output,
+        csq=[],
+        keep_if_any_damaging=True,
+        missense_filters=['Polyphen', 'sift=deleterious'],
     )
     results, expected = run_args(test_args, output,
                                  sys._getframe().f_code.co_name)
