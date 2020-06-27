2d7ef16dbcd6f52505b9222868b52d6ccbb4b0ef
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-06-28 00:38:54 +0100

    add bcf based vcf filter tests

--- test/test_vcf_filters.py
+++ test/test_vcf_filters.py
@@ -9,140 +9,152 @@ dbsnp = os.path.join(dir_path, "test_data", "dbSnpTest.vcf.gz")
 
 def teardown_module():
     for f in [vcf_filter, gnomad, dbsnp]:
-        idx = f + '.tbi'
-        if os.path.exists(idx):
-            os.remove(idx)
+        tbi = f + '.tbi'
+        csi = f.replace('.vcf.gz', '.bcf.csi')
+        for idx in [tbi, csi]:
+            if os.path.exists(idx):
+                os.remove(idx)
 
 
 def test_vcf_filter_known():
-    output = get_tmp_out()
-    test_args = dict(
-        vcf_filter=[vcf_filter + ',test_vcf'],
-        filter_known=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [vcf_filter, vcf_filter.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            vcf_filter=[f + ',test_vcf'],
+            filter_known=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                     sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_vcf_filter_novel():
-    output = get_tmp_out()
-    test_args = dict(
-        vcf_filter=[vcf_filter + ',test_vcf'],
-        filter_novel=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [vcf_filter, vcf_filter.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            vcf_filter=[f + ',test_vcf'],
+            filter_novel=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_vcf_filter_freq():
-    output = get_tmp_out()
-    test_args = dict(
-        vcf_filter=[vcf_filter + ',test_vcf'],
-        freq=0.1,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [vcf_filter, vcf_filter.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            vcf_filter=[f + ',test_vcf'],
+            freq=0.1,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_dbsnp_known():
-    output = get_tmp_out()
-    test_args = dict(
-        dbsnp=[dbsnp],
-        filter_known=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [dbsnp, dbsnp.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            dbsnp=[f],
+            filter_known=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                     sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_dbsnp_novel():
-    output = get_tmp_out()
-    test_args = dict(
-        dbsnp=[dbsnp],
-        filter_novel=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [dbsnp, dbsnp.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            dbsnp=[f],
+            filter_novel=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_dbsnp_freq():
-    output = get_tmp_out()
-    test_args = dict(
-        dbsnp=[dbsnp],
-        freq=0.005,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [dbsnp, dbsnp.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            dbsnp=[f],
+            freq=0.005,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_gnomad_novel():
-    output = get_tmp_out()
-    test_args = dict(
-        gnomad=[gnomad],
-        filter_novel=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [gnomad, gnomad.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            gnomad=[f],
+            filter_novel=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_gnomad_known():
-    output = get_tmp_out()
-    test_args = dict(
-        gnomad=[gnomad],
-        filter_known=True,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [gnomad, gnomad.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            gnomad=[f],
+            filter_known=True,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_gnomad_freq():
-    output = get_tmp_out()
-    test_args = dict(
-        gnomad=[gnomad],
-        freq=0.0005,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [gnomad, gnomad.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            gnomad=[f],
+            freq=0.0005,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_gnomad_homozygotes():
-    output = get_tmp_out()
-    test_args = dict(
-        gnomad=[gnomad],
-        freq=0.0005,
-        max_gnomad_homozygotes=0,
-        output=output,
-    )
-    results, expected = run_args(test_args, output,
-                                 sys._getframe().f_code.co_name)
-    assert_equal(results, expected)
-    os.remove(output)
+    for f in [gnomad, gnomad.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            gnomad=[f],
+            freq=0.0005,
+            max_gnomad_homozygotes=0,
+            output=output,
+        )
+        results, expected = run_args(test_args, output,
+                                    sys._getframe().f_code.co_name)
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 if __name__ == '__main__':
