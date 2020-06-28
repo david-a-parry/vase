fb454ca953d8286ff6633010dc061ed8704e5f4a
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-06-28 23:12:00 +0100

    Add tests for multi-chromosome lookups

--- test/test_vcf_filters.py
+++ test/test_vcf_filters.py
@@ -5,15 +5,50 @@ vcf_filter = os.path.join(dir_path,
                           "vcf_filter_test.vcf.gz")
 gnomad = os.path.join(dir_path, "test_data", "gnomadTest.vcf.gz")
 dbsnp = os.path.join(dir_path, "test_data", "dbSnpTest.vcf.gz")
+multichrom = os.path.join(dir_path, "test_data", "multi_chrom.vcf.gz")
 
 
 def teardown_module():
-    for f in [vcf_filter, gnomad, dbsnp]:
+    for f in [vcf_filter, gnomad, dbsnp, multichrom]:
         tbi = f + '.tbi'
         csi = f.replace('.vcf.gz', '.bcf.csi')
         for idx in [tbi, csi]:
             if os.path.exists(idx):
                 os.remove(idx)
+
+
+def test_identical_known():
+    for f in [multichrom, multichrom.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            input=multichrom,
+            vcf_filter=[f + ',test_vcf'],
+            filter_known=True,
+            output=output,
+        )
+        run_args(test_args)
+        with pysam.VariantFile(output) as vcf:
+            results = [var_string_from_record(x) for x in vcf]
+        assert_equal(results, [])
+        os.remove(output)
+
+
+def test_identical_novel():
+    for f in [multichrom, multichrom.replace('.vcf.gz', '.bcf')]:
+        output = get_tmp_out()
+        test_args = dict(
+            input=multichrom,
+            vcf_filter=[f + ',test_vcf'],
+            filter_novel=True,
+            output=output,
+        )
+        run_args(test_args)
+        with pysam.VariantFile(output) as vcf:
+            results = [var_string_from_record(x) for x in vcf]
+        with pysam.VariantFile(f) as vcf:
+            expected = [var_string_from_record(x) for x in vcf]
+        assert_equal(results, expected)
+        os.remove(output)
 
 
 def test_vcf_filter_known():
@@ -39,7 +74,7 @@ def test_vcf_filter_novel():
             output=output,
         )
         results, expected = run_args(test_args, output,
-                                    sys._getframe().f_code.co_name)
+                                     sys._getframe().f_code.co_name)
         assert_equal(results, expected)
         os.remove(output)
 
