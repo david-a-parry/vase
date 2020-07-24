d951c36a7babbb53d6ea8c3b9622cfd68ae5c6e9
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-07-24 09:56:39 +0100

    Add get_allele_balance method to avoid repetition; add gt_from_ad

--- vase/sv_gt_filter.py
+++ vase/sv_gt_filter.py
@@ -7,7 +7,8 @@ class SvGtFilter(object):
 
     __slots__ = ['gq', 'dp', 'max_dp', 'het_ab', 'hom_ab', 'gt_is_ok',
                  'ab_filter', 'ref_ab_filter', 'ad_over_threshold', 'fields',
-                 'enough_support', 'del_dhffc', 'dup_dhbfc', 'duphold_filter']
+                 'enough_support', 'del_dhffc', 'dup_dhbfc', 'duphold_filter',
+                 'get_allele_balance']
 
     def __init__(self, vcf, gq=0, dp=0, max_dp=0, het_ab=0., hom_ab=0.,
                  ref_ab_filter=None, del_dhffc=None, dup_dhbfc=None,
@@ -81,25 +82,36 @@ class SvGtFilter(object):
         self.ad_over_threshold = None
         self.enough_support = None
         self.duphold_filter = None
+        self.get_allele_balance = None
         ab_fields = None
         if not any([gq, dp, het_ab, hom_ab, del_dhffc, dup_dhbfc]):
             # if no parameters are set then every genotype passes
             self.gt_is_ok = lambda gt, smp, al: True
         else:
+            self.gt_is_ok = self._gt_is_ok
+        if het_ab or hom_ab or ref_ab_filter:
             ab_fields = self._check_header_fields(vcf)
             if ab_fields == ('PR', 'SR'):
                 # only option now, but may support other annotations in future
+                self.get_allele_balance = self._ab_from_prsr
                 self.ab_filter = self._ab_filter_prsr
                 self.enough_support = self._enough_support_prsr
             if dup_dhbfc or del_dhffc:
                 self.duphold_filter = self._duphold_filter
-            self.gt_is_ok = self._gt_is_ok
         if ref_ab_filter:
             if ab_fields is None:
                 ab_fields = self._check_header_fields(vcf)
             if ab_fields == ('PR', 'SR'):
                 # only option now, but may support other annotations in future
                 self.ad_over_threshold = self._alt_prsr_over_threshold
+
+    def _ab_from_prsr(self, gts, sample, allele):
+        support = self._get_pr_sr(gts, sample)
+        al_dp = support[allele]
+        dp = sum(support)
+        if dp > 0 and al_dp is not None:
+            return float(al_dp)/dp
+        return 0.0
 
     def _duphold_filter(self, gts, sample, allele, svtype):
         is_hom_alt = False
@@ -122,20 +134,13 @@ class SvGtFilter(object):
         return True  # do not filter
 
     def _alt_prsr_over_threshold(self, gts, sample, allele):
-        support = self._get_pr_sr(gts, sample)
-        al_dp = support[allele]
-        dp = sum(support)
-        if dp > 0 and al_dp is not None:
-            ab = float(al_dp)/dp
-            if ab > self.ref_ab_filter:
-                # ALT/REF supporting read counts > threshold
-                return True  # filter
+        ab = self.get_allele_balance(gts, sample, allele)
+        if ab is not None:
+            return ab > self.ref_ab_filter
         return False
 
     def _ab_filter_prsr(self, gts, sample, allele):
-        support = self._get_pr_sr(gts, sample)
-        al_dp = support[allele]
-        dp = sum(support)
+        ab = self.get_allele_balance(gts, sample, allele)
         is_hom_alt = False
         is_het_alt = False
         if len(set(gts[sample]['GT'])) == 1:
@@ -143,8 +148,7 @@ class SvGtFilter(object):
                 is_hom_alt = True
         elif allele in gts[sample]['GT']:
             is_het_alt = True
-        if al_dp is not None and dp > 0 and (is_het_alt or is_hom_alt):
-            ab = float(al_dp)/dp
+        if ab is not None and (is_het_alt or is_hom_alt):
             if is_het_alt and ab < self.het_ab:
                 return False  # filter
             if is_hom_alt and ab < self.hom_ab:
@@ -212,7 +216,7 @@ class SvGtFilter(object):
                                    "field is not defined in your VCF header.")
             self.fields.append("DHFFC")
         if (self.dp or self.max_dp or self.het_ab or self.hom_ab
-            or self.ref_ab_filter):
+                or self.ref_ab_filter):
             if ('PR' in vcf.header.header.formats and
                     'SR' in vcf.header.header.formats):
                 self.fields.append('PR')
@@ -225,5 +229,14 @@ class SvGtFilter(object):
                                    "defined in your VCF header.")
         return None
 
-    def gt_from_ad(self, gt):
-        raise NotImplementedError("GT from AD method not implmented yet!")
+    def gt_from_ad(self, gts, sample, allele):
+        if not self.het_ab:
+            raise ValueError("het_ab must be >0.0 if using gt_from_ad")
+        ab = self.get_allele_balance(gts, sample, allele)
+        if ab is None:
+            return (None, None)
+        if self.hom_ab and ab >= self.hom_ab:
+            return (allele, allele)
+        if ab >= self.het_ab:
+            return (0, allele)
+        return (0, 0)
