a569bd6eed10301418c95f0145b228e0a242ec2a
David A. Parry <david.parry@igmm.ed.ac.uk>
2021-01-11 15:02:11 +0000

    Allow generation of test data from inputs with chr prefix

--- test/utils/generate_test_data.py
+++ test/utils/generate_test_data.py
@@ -13,10 +13,19 @@ if __name__ == '__main__':
     vcf.subset_samples(samples)
     out = pysam.VariantFile("-", mode='w', header=vcf.header)
     pos_offset = 1000000
+    try:
+        first_chrom = vcf.index.keys()[0]
+    except AttributeError:
+        first_chrom = None
     for record in vcf:
+        if first_chrom is None:
+            if record.chrom.startswith('chr'):
+                first_chrom = 'chr1'
+            else:
+                first_chrom =  '1'
         if any(y is not None and y > 0 for x in samples for y in
                record.samples[x].allele_indices):
-            record.chrom = '1'
+            record.chrom = first_chrom
             record.pos = pos_offset + random.randint(10, 1e3)
             if random.randint(0, 10) % 2:
                 record.id = "snp{}".format(random.randint(1, 1e5))
