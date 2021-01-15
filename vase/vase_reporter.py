2f75ab3a53c6d8c30e431100747182d247256869
David A. Parry <david.parry@igmm.ed.ac.uk>
2021-01-15 15:50:38 +0000

    Add SnpEff support for vase_reporter

--- vase/vase_reporter.py
+++ vase/vase_reporter.py
@@ -28,6 +28,18 @@ feat_annots = {'VASE_biallelic_families': 'VASE_biallelic_features',
 impact_order = dict((k, n) for n, k in enumerate(['HIGH', 'MODERATE', 'LOW',
                                                   'MODIFIER']))
 
+vep_keys = {'biotype': 'BIOTYPE',
+            'impact': 'IMPACT',
+            'symbol': 'SYMBOL',
+            'gene': 'Gene',
+            'feature': 'Feature'}
+
+snpeff_keys = {'biotype': 'Transcript_Biotype',
+               'impact': 'Annotation_Impact',
+               'symbol': 'Gene_Name',
+               'gene': 'Gene_ID',
+               'feature': 'Feature_ID'}
+
 
 class VaseReporter(object):
     '''
@@ -72,6 +84,8 @@ class VaseReporter(object):
         self.choose_transcript = choose_transcript
         self.seg_fields = self._get_seg_fields()
         self.info_annotations = self._get_info_fields(info_fields)
+        self.csq_fields, self._snpeff_mode = self._get_csq_fields()
+        self._csq_keys = snpeff_keys if self._snpeff_mode else vep_keys
         if not out.endswith(".{}".format(output_type)):
             out = out + ".{}".format(output_type)
         if os.path.exists(out) and not force:
@@ -87,7 +101,7 @@ class VaseReporter(object):
         self.allelic_requirement = allelic_requirement
         self.mutation_requirement = mutation_requirement
         if g2p:
-            self.g2p = G2P(g2p)
+            self.g2p = G2P(g2p, snpeff_mode=self._snpeff_mode)
         elif self.require_g2p:
             raise RuntimeError("--filter_non_g2p option requires a G2P " +
                                "CSV to be supplied with the --g2p argument")
@@ -235,6 +249,22 @@ class VaseReporter(object):
                                    " by --singletons already exists " +
                                    "in PED file {}" .format(self.ped.filename))
 
+    def _get_csq_fields(self):
+        fields = None
+        snpeff_mode = False
+        try:
+            fields = self.vcf.header.csq_fields
+            self.logger.info("Found VEP annotations")
+        except KeyError:
+            try:
+                fields = self.vcf.header.ann_fields
+                self.logger.info("Found SnpEff annotations")
+                snpeff_mode = True
+            except KeyError:
+                raise KeyError("Neither VEP nor SnpEff annotations found in" +
+                               "input file {}".format(self.vcf.filename))
+        return fields, snpeff_mode
+
     def _get_seg_fields(self):
         inheritance_fields = dict()
         seg = False
@@ -315,7 +345,7 @@ class VaseReporter(object):
             header.append("CADD_PHRED_score")
         for inf in self.info_annotations.keys():
             header.append(inf)
-        header.extend(x for x in self.vcf.header.csq_fields if x != 'Allele')
+        header.extend(x for x in self.csq_fields if x != 'Allele')
         if self.rest_lookups:
             header.extend(["ENTREZ", "Full_Name", "GO", "REACTOME",
                            "MOUSE_TRAITS", "MIM_MORBID"])
@@ -403,23 +433,24 @@ class VaseReporter(object):
         return features
 
     def get_ensembl_rest_data(self, csq):
-        if not csq['Feature']:
+        if not csq[self._csq_keys['feature']]:
             return [''] * 5
-        if csq['Feature'] not in self.rest_cache:
-            if not ENST.match(csq['Feature']):
+        if csq[self._csq_keys['feature']] not in self.rest_cache:
+            if not ENST.match(csq[self._csq_keys['feature']]):
                 self.logger.warn("Skipping REST lookup of non-Ensembl " +
                                  "transcript feature " +
-                                 "'{}'".format(csq['Feature']))
-                self.rest_cache[csq['Feature']] = [''] * 5
+                                 "'{}'".format(csq[self._csq_keys['feature']]))
+                self.rest_cache[csq[self._csq_keys['feature']]] = [''] * 5
             else:
                 try:
-                    self.rest_cache[csq['Feature']] = self._get_rest_data(csq)
+                    self.rest_cache[csq[self._csq_keys['feature']]] = \
+                        self._get_rest_data(csq)
                 except Exception as err:
                     self.logger.warn(err)
                     self.logger.warn("REST lookups for {} failed".format(
-                                                               csq['Feature']))
-                    self.rest_cache[csq['Feature']] = [''] * 5
-        return self.rest_cache[csq['Feature']]
+                        csq[self._csq_keys['feature']]))
+                    self.rest_cache[csq[self._csq_keys['feature']]] = [''] * 5
+        return self.rest_cache[csq[self._csq_keys['feature']]]
 
     def _get_rest_data(self, csq):
         entrez = ''
@@ -429,18 +460,20 @@ class VaseReporter(object):
         traits = ''
         mim = ''
         try:
-            go_data = self.ensembl_rest.get_xref(csq['Feature'],
-                                                 external_db='GO')
+            go_data = self.ensembl_rest.get_xref(
+                csq[self._csq_keys['feature']], external_db='GO')
             if go_data:
                 go = str.join("|", (x['description'] for x in go_data if
                                     x['description'] is not None))
         except Exception as err:
             self.logger.warn(err)
-            self.logger.warn("GO lookup for {} failed".format(csq['Feature']))
+            self.logger.warn("GO lookup for {} failed".format(
+                csq[self._csq_keys['feature']]))
             go = 'LOOKUP FAILED'
-        if csq['Gene']:
+        if csq[self._csq_keys['gene']]:
             try:
-                xref_data = self.ensembl_rest.get_xref(csq['Gene'])
+                xref_data = self.ensembl_rest.get_xref(
+                    csq[self._csq_keys['gene']])
                 entrez = str.join("|", (x['primary_id'] for x in xref_data
                                         if x['dbname'] == 'EntrezGene'))
                 full_name = str.join("|", (x['description'] for x in xref_data
@@ -452,44 +485,47 @@ class VaseReporter(object):
             except Exception as err:
                 self.logger.warn(err)
                 self.logger.warn("XREF lookups for {} failed".format(
-                                                               csq['Feature']))
+                    csq[self._csq_keys['feature']]))
                 entrez = 'LOOKUP FAILED'
                 full_name = 'LOOKUP FAILED'
                 reactome = 'LOOKUP FAILED'
                 mim = 'LOOKUP FAILED'
             try:
-                orth = self.ensembl_rest.lookup_ortholog(csq['Gene'])
+                orth = self.ensembl_rest.lookup_ortholog(
+                    csq[self._csq_keys['gene']])
                 if orth is not None:
                     traits = str.join("|", self.ensembl_rest.get_traits(orth))
             except Exception as err:
                 self.logger.warn(err)
                 self.logger.warn("Orthology lookup for {} failed".format(
-                                                               csq['Feature']))
+                    csq[self._csq_keys['feature']]))
                 traits = 'LOOKUP FAILED'
         return [entrez, full_name, go, reactome, traits, mim]
 
     def get_mygene_data(self, csq):
-        if csq['Gene']:
-            if csq['Gene'] in self.mygene_cache:
-                return self.mygene_cache[csq['Gene']]
-            results = self.mg.query(csq['Gene'],
+        if csq[self._csq_keys['gene']]:
+            if csq[self._csq_keys['gene']] in self.mygene_cache:
+                return self.mygene_cache[csq[self._csq_keys['gene']]]
+            results = self.mg.query(csq[self._csq_keys['gene']],
                                     scopes='ensemblgene,entrezgene',
                                     species=9606, fields=",".join(MG_FIELDS))
             if len(results['hits']) == 0:
-                results = self.mg.query(csq['SYMBOL'],
+                results = self.mg.query(csq[self._csq_keys['symbol']],
                                         scopes='symbol',
                                         species=9606,
                                         fields=",".join(MG_FIELDS))
             if len(results['hits']) == 0:
                 self.logger.warn("No MyGene hits for gene {}/{}".format(
-                    csq['Gene'], csq['SYMBOL']))
-                self.mygene_cache[csq['Gene']] = [''] * 8
+                    csq[self._csq_keys['gene']],
+                    csq[self._csq_keys['symbol']]))
+                self.mygene_cache[csq[self._csq_keys['gene']]] = [''] * 8
                 return [''] * 8
             elif len(results['hits']) > 1:
                 self.logger.warn("Multiple ({}) ".format(
                                  len(results['hits'])) +
                                  "MyGene hits for gene {}/{}".format(
-                                     csq['Gene'], csq['SYMBOL']) +
+                                     csq[self._csq_keys['gene']],
+                                     csq[self._csq_keys['symbol']]) +
                                  " - will use first hit only.")
             data = []
             for field in MG_FIELDS:
@@ -515,7 +551,7 @@ class VaseReporter(object):
                                           results['hits'][0]['generif']]))
                 else:
                     data.append(results['hits'][0][field])
-                self.mygene_cache[csq['Gene']] = data
+                self.mygene_cache[csq[self._csq_keys['gene']]] = data
             return data
         return [''] * 8
 
@@ -528,7 +564,7 @@ class VaseReporter(object):
         if "ENTREZ_ID" in self._header_columns[family]:  # from MyGene
             entrez_cols.append(self._header_columns[family].index("ENTREZ_ID"))
         if "MIM" in self._header_columns[family]:  # from MyGene
-            mim_col =  self._header_columns[family].index("MIM")
+            mim_col = self._header_columns[family].index("MIM")
         for col in range(len(values)):
             if col in (entrez_cols):  # provide link to Entrez Gene
                 m = ENTREZ_RE.match(values[col])
@@ -550,35 +586,37 @@ class VaseReporter(object):
 
     def get_g2p_data(self, csq):
         d = [''] * 5
-        if csq['SYMBOL'] in self.g2p.g2p:
-            d = ["|".join(x[f] for x in self.g2p.g2p[csq['SYMBOL']]) for f in
+        if csq[self._csq_keys['symbol']] in self.g2p.g2p:
+            d = ["|".join(x[f] for x in
+                 self.g2p.g2p[csq[self._csq_keys['symbol']]]) for f in
                  ['disease name', 'DDD category', 'allelic requirement',
-                  'mutation consequence', 'organ specificity list']]
+                 'mutation consequence', 'organ specificity list']]
         return d
 
     def get_constraint_data(self, csq):
         cons = [''] * 5
         k = None
-        if csq['Feature'] in self.constraint:
-            k = csq['Feature']
-        elif csq['SYMBOL'] in self.constraint:
-            k = csq['SYMBOL']
+        if csq[self._csq_keys['feature']] in self.constraint:
+            k = csq[self._csq_keys['feature']]
+        elif csq[self._csq_keys['symbol']] in self.constraint:
+            k = csq[self._csq_keys['symbol']]
         if k is not None:
             cons = [self.constraint[k][f] for f in self.constraint_cols]
         return cons
 
-    def write_records(self, record, family, inheritance, allele, features):
-        for csq in (x for x in record.CSQ if x['Feature'] in features and
-                    x['alt_index'] == allele):
+    def write_records(self, record, csqs, family, inheritance, allele,
+                      features):
+        for csq in (x for x in csqs if x[self._csq_keys['feature']] in features
+                    and x['alt_index'] == allele):
             # column order is: Inheritance, vcf_output_columns, allele, AC, AN,
             #                  GTS, VEP fields
             if self.require_g2p:
-                if csq['SYMBOL'] not in self.g2p.g2p:
+                if csq[self._csq_keys['symbol']] not in self.g2p.g2p:
                     continue
                 if self.allelic_requirement:
                     inh_ok = False
                     for req in (x['allelic requirement'] for x in
-                                self.g2p.g2p[csq['SYMBOL']]):
+                                self.g2p.g2p[csq[self._csq_keys['symbol']]]):
                         for r in req.split(","):
                             if inheritance in allelic_req_to_label[r]:
                                 inh_ok = True
@@ -590,7 +628,7 @@ class VaseReporter(object):
                             csq, keep_uncertain=True):
                         continue
             if self.blacklist:
-                if csq['Feature'] in self.blacklist:
+                if csq[self._csq_keys['feature']] in self.blacklist:
                     continue
             values = [inheritance]
             values.extend(",".join(str(y) for y in x) if isinstance(x, tuple)
@@ -620,8 +658,7 @@ class VaseReporter(object):
                 except KeyError:
                     values.append('.')
             values.extend(self._add_info_annotations(record, allele))
-            values.extend(csq[x] for x in self.vcf.header.csq_fields if
-                          x != 'Allele')
+            values.extend(csq[x] for x in self.csq_fields if x != 'Allele')
             if self.rest_lookups:
                 values.extend(self.get_ensembl_rest_data(csq))
             if self.mygene_lookups:
@@ -650,16 +687,17 @@ class VaseReporter(object):
             as ordered in vase/data/biotypes.tsv and canonical
             transcripts where impacts are the same.
         '''
-        feat_csq = (x for x in csq if x['Feature'] in features and
-                    x['alt_index'] == allele)
-        sorted_csq = sorted(feat_csq, key=lambda x: x['CANONICAL'],
-                            reverse=True)
+        feat_csq = (x for x in csq if x[self._csq_keys['feature']] in features
+                    and x['alt_index'] == allele)
+        if 'CANONICAL' in csq[0]:
+            sorted_csq = sorted(feat_csq, key=lambda x: x['CANONICAL'],
+                                reverse=True)
         sorted_csq = sorted(sorted_csq, key=lambda x:
-                            self.biotype_order[x['BIOTYPE']])
+                            self.biotype_order[x[self._csq_keys['biotype']]])
         sorted_csq = sorted(sorted_csq, key=lambda x:
-                            impact_order[x['IMPACT']])
+                            impact_order[x[self._csq_keys['impact']]])
         if sorted_csq:
-            return sorted_csq[0]['Feature']
+            return sorted_csq[0][self._csq_keys['feature']]
         # none of the features are in the CSQ annotations (e.g. feature is a
         # genomic coordinate)
         return features[0]
@@ -670,24 +708,29 @@ class VaseReporter(object):
         n = 0
         for record in self.vcf:
             n += 1
+            if self._snpeff_mode:
+                csqs = record.ANN
+            else:
+                csqs = record.CSQ
             for annot, pattern in self.seg_fields.items():
                 if annot in record.info:
                     for i in range(len(record.info[annot])):
                         if record.info[annot][i] is None:
                             continue
                         if self.all_features:
-                            feat = list(x['Feature'] for x in record.CSQ if
-                                        x['Feature'] != '' and
+                            feat = list(x[self._feature_key] for x in csqs if
+                                        x[self._feature_key] != '' and
                                         x['alt_index'] == i + 1)
                         else:
-                            feat = record.info[self.feat_annots[annot]][i].split("|")
+                            feat = \
+                             record.info[self.feat_annots[annot]][i].split("|")
                         if self.choose_transcript:
                             feat = [self.pick_transcript(feat, i+1,
-                                                         record.CSQ)]
+                                                         csqs)]
                         for fam in record.info[annot][i].split("|"):
                             if fam in self.families:
-                                self.write_records(record, fam, pattern, i+1,
-                                                   feat)
+                                self.write_records(record, csqs, fam, pattern,
+                                                   i+1, feat)
             if n % self.prog_interval == 0:
                 self.logger.info("Parsed {:,} records".format(n))
         self.logger.info("Finished parsing {:,} records".format(n))
